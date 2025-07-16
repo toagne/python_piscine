76d22cbf256fa7477793d55e7d0b5b887b7b5a23
Marco pellegrini <marco.pellegrini1996@gmail.com>
2025-07-16 10:55:56 +0300

    fixed ex05 module01 - now using a copy array for the different shades

--- 01/ex05/pimp_image.py
+++ 01/ex05/pimp_image.py
@@ -3,34 +3,48 @@ import matplotlib.pyplot as plt
 
 def ft_invert(array):
     """Inverts the color of the image received"""
-    array[:, :, :] = 255 - array[:, :, :]
-    img = Image.fromarray(array)
+    arr = array.copy()
+    arr[:, :, :] = 255 - arr[:, :, :]
+    img = Image.fromarray(arr)
     plt.imshow(img)
+    plt.axis('off')
     plt.show()
 
 def ft_red(array):
-    array[:, :, 1] = 0
-    array[:, :, 2] = 0
-    img = Image.fromarray(array)
+    """Image is now in shades of red"""
+    arr = array.copy()
+    arr[:, :, 1] = 0
+    arr[:, :, 2] = 0
+    img = Image.fromarray(arr)
     plt.imshow(img)
+    plt.axis('off')
     plt.show()
 
 def ft_green(array):
-    array[:, :, 0] = 0
-    array[:, :, 2] = 0
-    img = Image.fromarray(array)
+    """Image is now in shades of green"""
+    arr = array.copy()
+    arr[:, :, 0] = 0
+    arr[:, :, 2] = 0
+    img = Image.fromarray(arr)
     plt.imshow(img)
+    plt.axis('off')
     plt.show()
 
 def ft_blue(array):
-    array[:, :, 0] = 0
-    array[:, :, 1] = 0
-    img = Image.fromarray(array)
+    """Image is now in shades of blue"""
+    arr = array.copy()
+    arr[:, :, 0] = 0
+    arr[:, :, 1] = 0
+    img = Image.fromarray(arr)
     plt.imshow(img)
+    plt.axis('off')
     plt.show()
 
 def ft_gray(array):
-    array = array[:, :, 1]
-    img = Image.fromarray(array)
+    """Image is now in shades of gray"""
+    arr = array.copy()
+    arr = array[:, :, 1]
+    img = Image.fromarray(arr)
     plt.imshow(img, cmap='gray')
+    plt.axis('off')
     plt.show()
